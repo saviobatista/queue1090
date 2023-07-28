1c19e8f693475923c6977edf1f943fc89b82b7f9
luciodaou <53634214+luciodaou@users.noreply.github.com>
2023-07-28 16:13:27 -0300

    Update queue1090_check.sh
    Adds a log on the folder (check.log)

--- queue1090_check.sh
+++ queue1090_check.sh
@@ -2,14 +2,15 @@
 
 # Checks if the files produced by queue1090 were modified more than 10 minutes ago, and if so, restarts the docker compose.
 
-## Add to /etc/crontab to run each 10 minutes
-# Queue1090
-# */10 * * * * root <path>/queue1090_check.sh
+# Add to /etc/crontab to run each 10 minutes
+#
+## Queue1090
+#*/10 * * * * root /opt/queue1090/queue1090_check.sh
 
 
 # Variables
-dockercompose="docker-compose"  # Check if adequate, on some systems, it's "docker compose"
-# dockercompose="docker compose"
+#dockercompose="docker-compose"  # Check if adequate, on some systems, it's "docker compose"
+dockercompose="docker compose"
 dir_log="/home/rj/log_hdd/adsbqueue"  # Path of log files
 dir_queue="/opt/queue1090"  # Path of queue1090
 period=10  # Lookup interval in minutes (integer)
@@ -28,14 +29,17 @@ if [ -f "${dir_log}/${file_name}" ]; then
   if (( $(date -d "${modified_time}" +%s) < $(date -d "now - ${period} minutes" +%s) )); then
 
     # Restart the docker compose
+    echo "Run on $(date) - ERROR - File: Exists, Log: Stalled" >> $dir_queue/check.log
     echo "Restart the docker compose at ${dir_queue}"
     cd "${dir_queue}" && ${dockercompose} restart
+    echo "Run on $(date) - ERROR - Docker Compose Restarted" >> $dir_queue/check.log
 
   else
 
     # The file was not modified in the last $period minutes, so exit the script
     echo "The file was not modified in the last $period minutes, so exit the script"
     ${dockercompose} ls
+    echo "Run on $(date) - OK - File: Exists - Log: Running " >> $dir_queue/check.log
     exit 0
 
   fi
@@ -43,7 +47,11 @@ if [ -f "${dir_log}/${file_name}" ]; then
 else
 
   # The file does not exist, so restart the docker compose
+  echo "Run on $(date) - ERROR - File: Missing, Log: N/A" >> $dir_queue/check.log
   echo "The file does not exist, so restart the docker compose at ${dir_queue}"
   cd "${dir_queue}" && ${dockercompose} restart
-
+  echo "Run on $(date) - ERROR - Docker Compose Restarted" >> $dir_queue/check.log
 fi
+
+
+echo "Run on $(date) - DEBUG >> $dir_queue/check.log
